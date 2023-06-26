0c71db50646c2c630b3929ea7b95c10cf681754b
rodalyn <rodalyndalmacio@gmail.com>
2023-06-26 10:45:30 -0700

    Initial Commit

---
+++ app/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Rodalyn Dalmacio on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack {
+            Text("What is your name?")
+            TextField("", text: $name)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.gray, width:1)
+        Button("Submit Name") {
+        textTitle = "W"
+            }
+        .font(.title2)
+        .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
